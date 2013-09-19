4ce70d62cecfa2d02000238a15da8681c41456e6
alanevwes <alanevwes@gmail.com>
2013-09-19 12:39:03 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Turf::Application.config.secret_key_base = '686c6db44b9e01191c4e7c03c019d5e2ee60585ff5206720ef301bdf6aa0ecdbee07e7d32f9862180b4e70bcc6f9b66336c986cd352b9a5f80dda62f93b1deb6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Turf::Application.config.session_store :cookie_store, key: '_turf_session'
