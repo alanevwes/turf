8444d252cd9d824ea3dcff2ff0fc07e1e1602fa7
alanevwes <alanevwes@gmail.com>
2013-09-20 14:07:21 +0200

    signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,23 +25,20 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
     end
   end
 
   private
-   def user_params
-     params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
-   end   
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end  
 
 
   # PATCH/PUT /users/1
@@ -75,7 +72,7 @@ class UsersController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:name, :email)
-    end
+    #def user_params
+    #  params.require(:user).permit(:name, :email)
+    #end
 end
