f27214403e99e7497f26e932b2c61cf6d5662482
alanevwes <alanevwes@gmail.com>
2013-10-12 16:21:29 +0200

    tallies amount

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @users = User.all
+    @tallies = @users.tallies.paginate(page: params[:page])
   end
 
   # GET /users/new
